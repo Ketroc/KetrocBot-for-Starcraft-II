f9a23294c0918b909b86a7c70210feaab482c038
Ketroc <21rocket@gmail.com>
2021-10-30 23:06:34 -0400

    -fixed a meme chat

--- src/main/java/com/ketroc/utils/Chat.java
+++ src/main/java/com/ketroc/utils/Chat.java
@@ -42,7 +42,7 @@ public class Chat {
 
     public static final List<String> WINNING_BM_CHAT = new ArrayList<>(List.of(
             "This is where Larva would start playing with his feet",
-            "Just GG out.  I'm not going to crash--                  0x6F09844E FATAL ERROR!",
+            "Just GG out.  I'm not going to crash--                            0x6F09844E FATAL ERROR!",
             "Ya give up, or ya thirsty for more?",
             "KET REKT!"
     ));
